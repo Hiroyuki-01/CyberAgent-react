ac10c22736d2a7b1a7cc03b8b3f1d85c06b1488c
Hiroyuki-01 <hiroyuki02228n@gmail.com>
2021-12-13 16:43:01 +0900

    first

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Task } from '../'
+
+export const TaskList = ({ tasks, setTasks }) => {
+  // Taskの状態を切り替える
+  const handleCheckBox = {
+    e: React.ChangeEvent<HTMLInputElement>,
+    l:number
+  } => {
+    const newTasks = tasks.map((task, i) => {
+      return i === i ? { ...task, isDone: e.target.checked} : task;
+    });
+    setTasks(newTasks);
+  };
+
+  return (
+    <ul>
+      {tasks.map((task, index) => (
+        <li key={'todo-${index}'}>
+          {task.isDone ? <s>{task.label}</s> : task.label}
+          <input
+            onChange={(e) => handleCheckBox{e, index}}
+            type='checkbox'
+            checked={task.isDone}
+            />
+        </li>
+      ))}
+    </ul>
+  );
+};
---
+++ src/index.jsx
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react";
+import {request} from "./server";
+import ReactDOM from "react-dom";
+import { TaskList } from './components/TaskList';
+import { TaskForm } from './components/TaskForm';
+
+const App = () => {
+  // タスクリストを格納する
+  const [tasks, setTasks] = useState<Task[]>([]);
+  // 追加前のタスクを格納する
+  const [newTaskLabel, setNewTaskLabel] = useState<String>('');
+
+  // ページマウント時にモックAPIからデータを取得
+  useEffect(() => {
+    request.fetchTasks((payload) => setTasks(payload)  );
+  }, []);
+
+  return (
+    <div style={{width:'780px', margin:'@ auto'}}>
+      {/* ヘッダー */}
+      <h1>Tutorial Works</h1>
+      <h2>React Todo List</h2>
+
+      {/* 一覧表示 */}
+      <TaskList {...{tasks, setTasks }} />
+
+      {/* タスク追加、削除 */}
+      <TaskForm {... { tasks, setTasks, newTaskLabel, setNewTaskLabel }} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#app"));
