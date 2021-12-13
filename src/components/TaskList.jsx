accfaac1dbd1e4115a6d0f5bb6845153d58b8971
Hiroyuki-01 <hiroyuki02228n@gmail.com>
2021-12-13 17:24:07 +0900

    debug error

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,10 +3,10 @@ import { Task } from '../'
 
 export const TaskList = ({ tasks, setTasks }) => {
   // Taskの状態を切り替える
-  const handleCheckBox = {
-    e: React.ChangeEvent<HTMLInputElement>,
-    l:number
-  } => {
+  const handleCheckBox = (
+    e,
+    l
+  ) => {
     const newTasks = tasks.map((task, i) => {
       return i === i ? { ...task, isDone: e.target.checked} : task;
     });
@@ -19,7 +19,7 @@ export const TaskList = ({ tasks, setTasks }) => {
         <li key={'todo-${index}'}>
           {task.isDone ? <s>{task.label}</s> : task.label}
           <input
-            onChange={(e) => handleCheckBox{e, index}}
+            onChange={(e) => handleCheckBox(e, index)}
             type='checkbox'
             checked={task.isDone}
             />
